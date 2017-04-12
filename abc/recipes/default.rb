a4b69303311e7d7c93a11af57eb22a02805495ff
root <root@server1.abc.com>
2017-04-12 08:40:20 +0530

    first

---
+++ abc/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: abc
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/etc/abc" do
+       source "abc.erb"
+end
---
+++ motd/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: motd
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+cookbook_file "/etc/motd" do
+ source "motd"
+end
